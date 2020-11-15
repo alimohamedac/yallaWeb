9db030ccb58bdbc1e0d37fb8b3b53e1201725932
Ali Mohamed Ali <am6064029@gmail.com>
2020-11-15 12:34:18 +0200

    handle theme files

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    @include('partials.head')
+</head>
+
+<body>
+
+    @include('partials.navbar')
+
+    @include('partials.content')
+
+    @include('partials.footer')
+
+
+
+<!-- Bootstrap core JavaScript -->
+<script src="{{ asset('themes/vendor/jquery/jquery.min.js') }}"></script>
+<script src="{{ asset('themes/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+
+</body>
+
+</html>
